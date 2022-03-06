9764b58406f1b1fcdd31f488bb7a99c3e12c2a6b
Georges034302 <georges.boughantous@gmail.com>
2022-03-06 22:47:29 +1100

    day5

--- week02/Scripts/create.sh
+++ week02/Scripts/create.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 #create n-files sequentially using touch
+#n is the number number of files passed as argument to the script
 
-n=$1
+current=$(date '+%d-%m-%Y') #get the current date
+folder=$(whoami)-$current   #set the directory name
+#mkdir $folder               #create the directory
+
 echo "Creating $n files ..."
+n=$1
+first=1
 
-for((i=1;i<=$n;i++))
+while true
 do
-	touch $(whoami)-$i.txt
+        if [ -e $(whoami)$first.txt ]
+        then
+                ((first++))
+        else
+                break
+        fi
 done
 
+for((i=$first-1;i<=$(($first+$n));i++))
+do
+       	if [ $i -eq 0 ]
+       	then
+		touch $(whoami).txt
+	else
+		touch $(whoami)$i.txt
+	fi
+done
+
+
+ls -l
