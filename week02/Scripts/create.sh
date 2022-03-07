89002e0616eaa8b4d6254e3485e31147148e4750
Georges034302 <georges.boughantous@gmail.com>
2022-03-07 16:22:53 +1100

    lab140-create

--- week02/Scripts/create.sh
+++ week02/Scripts/create.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
-#create n-files sequentially using touch
-#n is the number number of files passed as argument to the script
 
-current=$(date '+%d-%m-%Y') #get the current date
-folder=$(whoami)-$current   #set the directory name
-#mkdir $folder               #create the directory
+###Create n-files where n is an argument to the script
 
-echo "Creating $n files ..."
-n=$1
-first=1
+n=$1         #stores first argument in n
+first=1      #pointer to the last file number created
 
 while true
 do
-        if [ -e $(whoami)$first.txt ]
-        then
-                ((first++))
-        else
-                break
-        fi
-done
+	if [ -e $(whoami)$first.txt ]
+	then
+		((first++))
+	else
+		break
+	fi
+done  #this loop scans the current directory and checks for
+      #existing files and update first at every iteration
+      #this loop stops when name.number.txt does not exist
 
-for((i=$first-1;i<=$(($first+$n));i++))
+
+for((i=$first-1;i<$(($first+$n))-1;i++))
 do
-       	if [ $i -eq 0 ]
-       	then
+	if [ $i -eq 0 ]
+	then
 		touch $(whoami).txt
 	else
 		touch $(whoami)$i.txt
 	fi
 done
 
+var=$(date '+%d-%m-%Y')
+folder=$(whoami)-$var
+mkdir $folder
 
 ls -l
