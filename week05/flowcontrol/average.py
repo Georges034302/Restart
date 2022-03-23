3e03cc1bdf05d2450e361986f35e38f2c916fccf
Georges034302 <georges.boughantous@gmail.com>
2022-03-23 16:34:51 +1100

    w5d3

--- week05/flowcontrol/average.py
+++ week05/flowcontrol/average.py
@@ -2,11 +2,11 @@
 #Show the average value
 
 value = input("Value = ")
-number = 0
-if (value.isdigit()):
-    number = int(value)
-else:
+while (not(value.isdigit())):
+    print("Not number - Please try again ...") 
     value = input("Value = ")
+    
+number = int(value)
 
 total = 0
 count = 0
@@ -15,11 +15,10 @@ while number != -1:
     total += number
     count += 1
     value = input("Value = ")
-    if(not(value.isdigit()) and (value != -1)):
-        print("Please try again ...")  
+    while(not(value.isdigit())):
+        print("Not number - Please try again ...")  
         value = input("Value = ")
     number = int(value)
-
 
 print("Average value = {:.3f}".format(total/count))
     
